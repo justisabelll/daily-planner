00bc4024335bcc06b2e0493e90c5b1117b9fb66e
justisabelll <roywashington1547@gmail.com>
2024-10-16 00:09:40 -0400

    init + starting ui

---
+++ README.md
@@ -0,0 +1,15 @@
+# daily-planner
+
+a sveltekit version of a simple day planner (todo list) i made [here](https://github.com/justisabelll/day-planner)
+
+install dependencies
+
+```bash
+bun install
+```
+
+run the dev server
+
+```bash
+bun run dev
+```
---
+++ src/components/header.svelte
@@ -0,0 +1,12 @@
+<div class="m-4 p-4 rounded-2xl border border-gray-300/50 shadow-sm">
+  <div class="flex justify-between items-center">
+    <p class="text-4xl font-thinner">Daily Planner</p>
+    <p class="text-xl font-light">
+      {new Date().toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+      })}
+    </p>
+  </div>
+</div>
