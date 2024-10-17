d7d4f6c3d83145e383882ebaca3c89916ccee8dd
justisabelll <roywashington1547@gmail.com>
2024-10-17 03:41:52 -0400

    schema update

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,20 +1,14 @@
-import {
-  mysqlTable,
-  varchar,
-  int,
-  datetime,
-  boolean,
-} from 'drizzle-orm/mysql-core';
+import { mysqlTable, varchar, int, datetime, boolean } from 'drizzle-orm/mysql-core';
 
 import { sql } from 'drizzle-orm';
 
 export const tasks = mysqlTable('tasks', {
-  id: int('id').primaryKey().notNull().autoincrement(),
-  task: varchar('task', { length: 255 }).notNull(),
-  completed: boolean('completed').default(false),
-  createdAt: datetime('created_at')
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
+	id: int('id').primaryKey().notNull().autoincrement(),
+	task: varchar('task', { length: 255 }).notNull(),
+	completed: boolean('completed').default(false).notNull(),
+	createdAt: datetime('created_at')
+		.default(sql`CURRENT_TIMESTAMP`)
+		.notNull()
 });
 
 export type Task = typeof tasks.$inferSelect;
