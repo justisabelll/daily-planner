15a850832789fded1238f4dfd81c4f0be836435b
justisabelll <roywashington1547@gmail.com>
2024-10-18 04:05:30 -0500

    more logging

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,8 +5,8 @@ const dbURL =
 	process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.DEV_DATABASE_URL;
 
 if (!dbURL) {
-	console.log('DB URL:', dbURL);
-	throw new Error(`${process.env.NODE_ENV} DATABASE_URL is not set`);
+	console.log('DB URL:', dbURL, 'env:', process.env.NODE_ENV);
+	throw new Error('DATABASE_URL is not set');
 }
 
 const db = drizzle({ connection: { uri: dbURL } });
